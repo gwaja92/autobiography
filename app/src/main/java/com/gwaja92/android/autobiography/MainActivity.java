c1c80d436fc7e1b803d69b907b59057eb967a20d
gwaja92 <gwaja92@gmail.com>
2021-06-21 16:36:48 +0900

    2nd commit

--- app/src/main/java/com/gwaja92/android/autobiography/MainActivity.java
+++ app/src/main/java/com/gwaja92/android/autobiography/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gwaja92.android.autobiography;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar bar = getSupportActionBar();
+        bar.hide();
     }
 }
